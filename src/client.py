1762b0d4888e665f28242fd36ac856b2603c2762
CaptainCluster <sranta.ville@gmail.com>
2025-03-12 12:04:57 +0200

    Wikipedia queries work

--- src/client.py
+++ src/client.py
@@ -1,6 +1,7 @@
 import xmlrpc.client
 from config.settings import Settings
 from datetime import datetime
+import xml.etree.ElementTree as ET
 
 settings = Settings()
 
@@ -52,29 +53,75 @@ def request_read(proxy):
     return proxy.read_note(note_topic)
 
 def request_wikipedia(proxy):
-    wiki_topic = input("The topic of the article: ")
-    wikipedia_search_results = proxy.search_wikipedia(wiki_topic)
-
-    # No results? Then the function will return
-    if len(wikipedia_search_results) == 0:
-        print("No articles found from Wikipedia. Try another topic.")
-        return
-    
-    print("The Wikipedia search yields the following potential articles:")
-    for i in range(1, len(wikipedia_search_results)+1):
-        print(f"{i}) {wikipedia_search_results[i-1]}")
-
     try:
+        wiki_article_name = input("The name of the article: ")
+        wikipedia_search_results = proxy.search_wikipedia(wiki_article_name)
+
+        # No results? Then the function will return
+        if len(wikipedia_search_results) == 0:
+            print("No articles found from Wikipedia. Try another topic.")
+            return
+    
+        print("The Wikipedia search yields the following potential articles:")
+        for i in range(1, len(wikipedia_search_results)+1):
+            print(f"{i}) {wikipedia_search_results[i-1]}")
+
         while True:
-            chosen_article = int(input("Which one will you choose: "))
-            if chosen_article < 1 or chosen_article > len(wikipedia_search_results):
-                print(f"The input '{chosen_article}' is not within the bounds (upper cap is {len(wikipedia_search_results)})")
+            chosen_article_index = int(input("Which one will you choose: "))
+            if chosen_article_index < 1 or chosen_article_index > len(wikipedia_search_results):
+                print(f"The input '{chosen_article_index}' is not within the bounds (upper cap is {len(wikipedia_search_results)})")
                 continue
             break
 
+        wikipedia_result = proxy.fetch_summary(wikipedia_search_results[chosen_article_index-1])
+
+        # Handling in case the server sends an error
+        summary = wikipedia_result["summary"]
+        article_url = wikipedia_result["article_url"]
+
+        if (not summary or len(summary) == 0) and (not article_url or len(article_url) == 0):
+            print("Failed to fetch an adequate result! Try another topic.")
+            return
+
+        print("\nFound the following content:\n")
+        print(f"\nURL: {article_url}")
+        print(f"\nSummary: {summary}\n")
+
+
+        while True:
+            confirmation = input("Would you like to write the summary? y=yes, n=no ")
+            print(f"you gave '{confirmation}'")
+            if confirmation.lower() != "n" and confirmation.lower() != "y":
+                print("You gave an incorrect input! y=yes, n=no!")
+                continue
+            if confirmation.lower() == "n":
+                print("Cancelling the procedure.")
+                return
+            break
+        
+        text_content = f"""
+        URL: {article_url}
+
+        Summary: {summary}
+        """
+        
+        note_topic = input("What will be the topic of the note: ")
+        
+        note_dict = {
+            "topic":        note_topic,
+            "name":         wiki_article_name,
+            "text":         text_content,
+            "timestamp":    datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+        }
+        return proxy.write_note(note_dict)
+
+
     except Exception as exception:
         print(f"Error in function 'request_wikipedia': {exception}")
-            
+
+def read_xml(xml_content):
+    print("hi")
+
 
 def main():
     proxy = xmlrpc.client.ServerProxy(f"http://{settings.HOST}:{settings.PORT}")
--- src/server.py
+++ src/server.py
@@ -31,18 +31,38 @@ def read_note(note_topic):
             args=[note_topic, results, lock])
     thread.start()
     thread.join()
-    for i in results:
-        print(f"entry - {i}")
     return results
 
+def search_wikipedia(wiki_topic):
+    results = []
+    thread = threading.Thread(
+        target=wikipedia_handler.search_wikipedia,
+        args=[wiki_topic, results, lock]
+    )
+    thread.start()
+    thread.join()
+    return results[0]
+
+def fetch_summary(wiki_article_name):
+    results = []
+    thread = threading.Thread(
+        target=wikipedia_handler.fetch_summary,
+        args=[wiki_article_name, results, lock]
+    )
+    thread.start()
+    thread.join()
+    return results[0]
+
+
 def notify_server_start():
-    print(f"XML-rpc is serving on port {settings.PORT}.")
+    print(f"The server is serving on port {settings.PORT}.")
 
 def main():
     server = SimpleXMLRPCServer((settings.HOST, settings.PORT), requestHandler=RequestHandler)
     server.register_function(write_note, "write_note")
     server.register_function(read_note, "read_note")
-    server.register_function(wikipedia_handler.search_wikipedia, "search_wikipedia")
+    server.register_function(search_wikipedia, "search_wikipedia")
+    server.register_function(fetch_summary, "fetch_summary")
     notify_server_start()
     server.serve_forever()
 
